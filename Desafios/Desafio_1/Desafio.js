3802b302d682d194ac21e52416a0e0328c8435c7
Math3200 <96210828+Math3200@users.noreply.github.com>
2023-12-01 11:47:09 -0300

    1.0.0

---
+++ Desafios/Desafio_1/Desafio.js
@@ -0,0 +1,11 @@
+// Resolução básica
+
+let combustivel = 5;
+let consumo = 12;
+let distancia = 300;   
+    
+Custo_Deslocamento = (distancia/consumo)*combustivel;
+
+console.log(Custo_Deslocamento);
+
+//Resolução com input
---
+++ JS/variaveis_e_operadores.js
@@ -0,0 +1,10 @@
+// Variáveis
+let var1 = 10; //Muda de valor
+const var2 = 10; //Valor constante 
+
+console.log (var2);
+
+
+//Operadores básicos: +,-,*,/
+
+let var3 = 10 + 30;
